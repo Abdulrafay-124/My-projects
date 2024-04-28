6926053db0b53e299165276c8bda1f72c356663d
Abdulrafay-124 <139597623+Abdulrafay-124@users.noreply.github.com>
2024-04-28 09:50:48 +0800

    Create 100ylater.py

---
+++ 100ylater.py
@@ -0,0 +1,5 @@
+nameU = input("What's your name ? : ")
+ageU = int(input("How old are you ? : "))
+current_year = 2024
+Age_later = current_year - ageU + 100
+print(f"{nameU} you will turn 100 years old in {Age_later}")
