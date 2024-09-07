df29435a3858cad7d086fe80844446b0b2b544d9
Rafay <139597623+Abdulrafay-124@users.noreply.github.com>
2024-09-07 12:20:04 +0800

    Create oddnums.java
    prints out odd numbers till 99.

---
+++ oddnums.java
@@ -0,0 +1,16 @@
+public class A1Q3 {
+    
+    public static void main(String[] args){
+        
+         for (int i = 1; i <= 99; i = i + 2) {
+        System.out.println(i);
+    }
+        
+        
+    }
+    
+    
+   
+    
+
+    
